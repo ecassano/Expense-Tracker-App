1218acf61dd0a9c579c990075bd75b2c5d05d871
eduardosa54 <eduardo.sa@rio.rj.gov.br>
2024-02-20 11:14:03 -0300

    first commit

---
+++ screens/ManageExpenses.tsx
@@ -0,0 +1,13 @@
+import { StyleSheet, Text, View } from 'react-native';
+
+const ManageExpenses = () => {
+  return (
+    <View>
+      <Text>ManageExpenses</Text>
+    </View>
+  )
+}
+
+export default ManageExpenses
+
+const styles = StyleSheet.create({})
